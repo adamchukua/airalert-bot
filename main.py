58a56e0cb1167f255d3a548cad878144f81a0693
thegradle <adamchuk.v.1@gmail.com>
2022-03-19 11:09:08 +0200

    Read all new messages from channel

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from telethon import TelegramClient, sync
+from telethon import TelegramClient, sync, events
 from secret import api_id, api_hash
 
 client = TelegramClient('hp_ubuntu', api_id, api_hash)
+
+@client.on(events.NewMessage(chats=('fuufudkye'))) # air_alert_ua
+async def normal_handler(event):
+    print(event.message.to_dict()['message'])
+
 client.start()
+client.run_until_disconnected()
