089e3b93a5a901fe1229e1daa58cfd800cd1f946
thegradle <adamchuk.v.1@gmail.com>
2022-03-19 12:02:42 +0200

    Connect the bot and send alert messages to grop by id

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from telethon import TelegramClient, sync, events
-from secret import api_id, api_hash
+from telethon import TelegramClient, events
+from secret import api_id, api_hash, TOKEN
+import telebot
 
 client = TelegramClient('hp_ubuntu', api_id, api_hash)
+bot = telebot.TeleBot(TOKEN)
 
 @client.on(events.NewMessage(chats=('fuufudkye'))) # air_alert_ua
 async def normal_handler(event):
     message = event.message.to_dict()['message']
     if '#м_Миколаїв_та_Миколаївська_територіальна_громада' in message and '🔴' in message:
-        print('Тривога!')
+        bot.send_message(-686742350, "Тривога!")
 
 client.start()
 client.run_until_disconnected()
+bot.infinity_polling()
