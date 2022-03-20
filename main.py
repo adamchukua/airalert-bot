6ba35cfab7f47b30db336470ad25ac5ee7fabbcf
thegradle <adamchuk.v.1@gmail.com>
2022-03-20 10:32:40 +0200

    Add the are to monitor for air alerts

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from telethon import TelegramClient, events
 from secret import api_id, api_hash, TOKEN, group_id
 import telebot
 
-client = TelegramClient('hp_ubuntu', api_id, api_hash)
+client = TelegramClient('app', api_id, api_hash)
 bot = telebot.TeleBot(TOKEN)
 
 @client.on(events.NewMessage(chats=('air_alert_ua')))
 async def normal_handler(event):
     message = event.message.to_dict()['message']
-    if '#м_Миколаїв_та_Миколаївська_територіальна_громада' in message and '🔴' in message:
+    if '#м_Миколаїв_та_Миколаївська_територіальна_громада' or '#Миколаївська_область' in message and '🔴' in message:
         bot.send_photo(group_id, open('img.jpg', 'rb'))
 
 client.start()
