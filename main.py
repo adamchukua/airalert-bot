433864aa2bc63d454c1b22def2b51f1e54cbaf5d
thegradle <adamchuk.v.1@gmail.com>
2022-03-25 14:49:14 +0200

    Add more media, add logging

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from telethon import TelegramClient, events
-from secret import api_id, api_hash, TOKEN, group_id
+import os
+import random
+
 import telebot
+from telethon import TelegramClient, events
+
+from secret import api_id, api_hash, TOKEN, group_id
 
 client = TelegramClient('app_v1', api_id, api_hash)
 bot = telebot.TeleBot(TOKEN)
@@ -11,10 +15,19 @@ async def alert_handler(event):
     message = event.message.to_dict()['message']
     if '#м_Миколаїв_та_Миколаївська_територіальна_громада' in message or '#Миколаївська_область' in message:
         if '🔴' in message:
-            bot.send_photo(group_id, open('media/alert_on/1.jpg', 'rb'), caption='‍Увага! Повітряна тривога!')
+            media = random.choice(os.listdir('media/alert_on/'))
+            if media[-3:] == 'jpg':
+                bot.send_photo(group_id, open('media/alert_on/' + media, 'rb'), caption='‍Увага! Повітряна тривога!')
+            else:
+                bot.send_video(group_id, open('media/alert_on/' + media, 'rb'), caption='‍Увага! Повітряна тривога!')
         elif '🟢' in message:
-            bot.send_photo(group_id, open('media/alert_off/1.jpg', 'rb'), caption='Увага! Відбій повітряної тривоги!')
-
+            media = random.choice(os.listdir('media/alert_off/'))
+            if media[-3:] == 'jpg':
+                bot.send_photo(group_id, open('media/alert_off/' + media, 'rb'), caption='Увага! Відбій повітряної тривоги!')
+            else:
+                bot.send_video(group_id, open('media/alert_off/' + media, 'rb'), caption='Увага! Відбій повітряної тривоги!')
+    else:
+        print(f'{message[2:7]}: bot is working')
 
 client.start()
 client.run_until_disconnected()
